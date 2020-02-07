2581e412edff096aa51fc80ab4fb5f0b4ea2de57
hidjo <hidjo@DESKTOP-5SIJPMQ>
2020-02-07 16:15:21 +0900

    my first commit

---
+++ HRM/src/main/MainFrame.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+
+public class MainFrame extends Frame{
+
+	private Button btn;
+	public MainFrame() {
+		// TODO Auto-generated constructor stub
+		
+		btn = new Button("¹öÆ°1");
+		setLayout(new FlowLayout());
+		add(btn);
+		setBounds(100,100,1280,768);
+		setVisible(true);
+	}
+	
+}
