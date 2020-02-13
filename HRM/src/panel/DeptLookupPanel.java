997d569c738fd15349bca92dee0458800a856382
user <user@Server09>
2020-02-13 21:30:55 +0900

    LHW - 부서조회

--- HRM/src/panel/DeptLookupPanel.java
+++ HRM/src/panel/DeptLookupPanel.java
@@ -1,31 +1,24 @@
 package panel;
 
-import java.awt.Color;
 import java.awt.Component;
-import java.awt.GridLayout;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Vector;
 
-import javax.swing.AbstractCellEditor;
 import javax.swing.DefaultCellEditor;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
-import javax.swing.table.AbstractTableModel;
+import javax.swing.UIManager;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableCellEditor;
-import javax.swing.table.TableColumn;
+import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumnModel;
 
-import org.w3c.dom.views.AbstractView;
 
 /**
  * @brief 부서조회
@@ -47,9 +40,9 @@ public class DeptLookupPanel extends JPanel{
 	private JTextArea area_explanation;
 	
 	//테이블 설정
-	private DeptTableModel tbModel_leader;
-	private DeptTableModel tbModel_deptList;
-	private DeptTableModel tbModel_empList;
+	private DefaultTableModel tbModel_leader;
+	private DefaultTableModel tbModel_deptList;
+	private DefaultTableModel tbModel_empList;
 
 	private JScrollPane scroll_leader;
 	private JScrollPane scroll_dept;
@@ -74,6 +67,9 @@ public class DeptLookupPanel extends JPanel{
 		
 		//사원 테이블 설정
 		makeEmpTable();
+		
+		//부서 설명
+		makeDeptExplanation();
 	
 		add(pane_main);
 
@@ -97,11 +93,22 @@ public class DeptLookupPanel extends JPanel{
 		Vector<Object> colNames = new Vector<>();
 		colNames.addElement("부서명");
 		
-		// 로우값 넣기
-		List<Vector> list = new ArrayList<Vector>();
+		// 로우값 넣기(테스트 데이터)
+		Vector<String> row1;
+		row1 = new Vector<>();
+		row1.addElement("인사팀");
+		Vector<String> row2;
+		row2 = new Vector<>();
+		row2.addElement("개발팀");
+		Vector<String> row3;
+		row3 = new Vector<>();
+		row3.addElement("영업팀");
 		
 		//테이블 모델 설정
-		tbModel_deptList = new DeptTableModel(colNames, list);
+		tbModel_deptList = new DefaultTableModel(colNames, 0);
+		tbModel_deptList.addRow(row1);
+		tbModel_deptList.addRow(row2);
+		tbModel_deptList.addRow(row3);
 		
 		//테이블 설정	
 		table_deptList = new JTable(tbModel_deptList);
@@ -111,11 +118,11 @@ public class DeptLookupPanel extends JPanel{
 		
 		table_deptList.setRowHeight(30);
 		colModel_dept = table_deptList.getColumnModel();
-		colModel_dept.getColumn(0).setPreferredWidth(150);
+		colModel_dept.getColumn(0).setPreferredWidth(100);
 		
 		//스크롤 설정	
-		scroll_dept = new JScrollPane(table_deptList);
-		scroll_dept.setBounds(50,100,150,300);
+		scroll_dept = new JScrollPane(table_deptList,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scroll_dept.setBounds(70,50,250,300);
 		
 		pane_main.add(scroll_dept);
 
@@ -124,25 +131,24 @@ public class DeptLookupPanel extends JPanel{
 	public void makeLeaderTable() {
 				
 		// 컬럼명 백터
-		Vector<Object> colNames = new Vector<>();
+		Vector<String> colNames = new Vector<>();
 		colNames.addElement("직급");
 		colNames.addElement("이름");
 		colNames.addElement("사번");
 		colNames.addElement("상세보기");
 		
-		// 로우값 넣기
-		List<Vector> list = new ArrayList<Vector>();
-		Vector<Object> row;
+		// 로우값 넣기(테스트 데이터)
+		Vector<String> row;
 		row = new Vector<>();
 		row.addElement("팀장");
 		row.addElement("김영희");
 		row.addElement("10004");
-		row.addElement(new JButton());
-		list.add(row);
-			
+		row.addElement("보기");
+		
 		// 테이블 모델 설정
-		tbModel_leader = new DeptTableModel(colNames,list);
-
+		tbModel_leader = new DefaultTableModel(colNames,0);
+		tbModel_leader.addRow(row);
+		
 		// 테이블 설정
 		table_leader = new JTable(tbModel_leader);
 		table_leader.setFillsViewportHeight(false);
@@ -150,7 +156,8 @@ public class DeptLookupPanel extends JPanel{
 		table_leader.setCellSelectionEnabled(false);
 		
 		// 셀 에디터 설정
-		table_leader.setDefaultEditor(JButton.class, new ButtonEditor());
+		table_leader.getColumn("상세보기").setCellEditor(new ButtonEditor(new JCheckBox()));
+		table_leader.getColumn("상세보기").setCellRenderer(new ButtonRenderer());
 		
 		// 셀 사이즈 설정
 		table_leader.setRowHeight(30);
@@ -161,7 +168,7 @@ public class DeptLookupPanel extends JPanel{
 		colModel_leader.getColumn(3).setPreferredWidth(50);
 		
 		//스크롤 설정
-		scroll_leader = new JScrollPane(table_leader);
+		scroll_leader = new JScrollPane(table_leader,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		scroll_leader.setBounds(600,30,600,55);
 		
 		pane_main.add(scroll_leader);
@@ -177,24 +184,47 @@ public class DeptLookupPanel extends JPanel{
 		colNames.addElement("사번");
 		colNames.addElement("상세보기");
 		
-		// 로우값 넣기
-		List<Vector> list = new ArrayList<Vector>();
-		Vector<Object> row;
-		row = new Vector<>();
-		row.addElement("사원");
-		row.addElement("홍길동");
-		row.addElement(new Boolean(true));
-		row.addElement(new JButton());
-		list.add(row);
-		
+		// 로우값 넣기(테스트 데이터)
+		Vector<Object> row1;
+		row1 = new Vector<>();
+		row1.addElement("사원");
+		row1.addElement("홍길동");
+		row1.addElement("10002");
+		row1.addElement("보기");		
+		Vector<Object> row2;
+		row2 = new Vector<>();
+		row2.addElement("사원");
+		row2.addElement("김동수");
+		row2.addElement("10003");
+		row2.addElement("보기");	
+		Vector<Object> row3;
+		row3 = new Vector<>();
+		row3.addElement("사원");
+		row3.addElement("대나무");
+		row3.addElement("10005");
+		row3.addElement("보기");	
+		Vector<Object> row4;
+		row4 = new Vector<>();
+		row4.addElement("사원");
+		row4.addElement("줄리앙");
+		row4.addElement("10011");
+		row4.addElement("보기");
 		
 		// 테이블 모델 설정
-		tbModel_empList = new DeptTableModel(colNames,list);
-
+		tbModel_empList = new DefaultTableModel(colNames,0);
+		tbModel_empList.addRow(row1);
+		tbModel_empList.addRow(row2);
+		tbModel_empList.addRow(row3);
+		tbModel_empList.addRow(row4);
+		
 		// 테이블 설정
 		table_empList = new JTable(tbModel_empList);
 		table_empList.setFillsViewportHeight(false);
 		table_empList.setRowSelectionAllowed(true);
+		
+		// 셀 에디터 설정
+		table_empList.getColumn("상세보기").setCellEditor(new ButtonEditor(new JCheckBox()));
+		table_empList.getColumn("상세보기").setCellRenderer(new ButtonRenderer());
 
 		table_empList.setRowHeight(30);
 		colModel_emp = table_empList.getColumnModel();
@@ -204,129 +234,100 @@ public class DeptLookupPanel extends JPanel{
 		colModel_emp.getColumn(3).setPreferredWidth(50);
 
 		// 스크롤 설정
-		scroll_emp = new JScrollPane(table_empList);
+		scroll_emp = new JScrollPane(table_empList,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		scroll_emp.setBounds(600, 150, 600, 400);
+		
 
 		pane_main.add(scroll_emp);
 
 	}
 	
-	//커스텀 테이블 모델
-	private class DeptTableModel extends AbstractTableModel{
+	public void makeDeptExplanation() {
 		
-		private Vector<Object> colNames; //테이블 컬럼 헤더
-		private List<Vector> rowList;  //테이블 로우 데이터
-		
-		public DeptTableModel(Vector<Object> names,List<Vector> list) {
-			// TODO Auto-generated constructor stub			
-			colNames = names;
-			rowList = list;
-		}
-		
-		//열 수 얻기
-		@Override
-		public int getColumnCount() {
-			// TODO Auto-generated method stub
-			return colNames.size();
-		}
-
-		//행 수 얻기
-		@Override
-		public int getRowCount() {
-			// TODO Auto-generated method stub
-			return rowList.size();
-		}
-
-		//열 헤더이름 얻기
-		@Override
-		public String getColumnName(int arg0) {
-			// TODO Auto-generated method stub
-			return (String)colNames.get(arg0);
-		}
-		
-		//셀 값 얻기
-		@Override
-		public Object getValueAt(int rowIndex, int columnIndex) {
-			// TODO Auto-generated method stub
-			return rowList.get(rowIndex).get(columnIndex);
-		}
-		
-		//열 클래스 얻기
-		@Override
-		public Class<?> getColumnClass(int columnIndex) {
-			// TODO Auto-generated method stub
-			return getValueAt(0, columnIndex).getClass();
-		}
-		
-		//수정불가
-		@Override
-		public boolean isCellEditable(int rowIndex, int columnIndex) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-		
-		//셀 수정
-		@Override
-		public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-			// TODO Auto-generated method stub
-			rowList.get(rowIndex).setElementAt(aValue, columnIndex);
-		}
-		
-		//열 추가
-		public void addRow(Vector<Object> row) {
-			rowList.add(row);
-		}
-		
-		//열 삭제
-		public void deleteRow(int rowIndex) {
-			rowList.remove(rowIndex);
-		}
-		
-		//출력
-		public void debugData() {
-			for(int i =0 ; i < rowList.size() ; i++) {
-				System.out.print("["+i+"] row :");
-				for(int j=0 ; j <colNames.size() ; j++) {
-					System.out.print(" "+rowList.get(i).get(j).toString());
-				}
-				System.out.println();
-			}
-		}
 	}
-
-	private class ButtonEditor extends AbstractCellEditor implements ActionListener, TableCellEditor {
+	
+	private class ButtonEditor extends DefaultCellEditor {
 
 		private JButton btn;
-		private ImageIcon icon;
-		private Color color = Color.PINK;
-		public ButtonEditor() {
+		private String label;
+		private boolean isPushed;
+		
+		public ButtonEditor(JCheckBox checkBox) {
 			// TODO Auto-generated constructor stub
+			super(checkBox);
 			btn = new JButton();
-			btn.setActionCommand("EDIT");
-			btn.addActionListener(this);
-			btn.setBackground(color);
-			btn.setBorderPainted(false);
+			btn.setOpaque(true);
+			btn.addActionListener(new ActionListener() {				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					fireEditingStopped();
+				}
+			});
 		}
+		
+
 		
 		@Override
 		public Object getCellEditorValue() {
 			// TODO Auto-generated method stub
-			return 	color;
-
+			if(isPushed) {
+				JOptionPane.showMessageDialog(btn,"버튼 눌림!");
+			}
+			isPushed = false;
+			return label;
 		}
 
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
-		}
 
 		@Override
 		public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row,
 				int column) {
 			// TODO Auto-generated method stub
-			color = (Color)value;
-			return btn;
+			if(isSelected) {
+				btn.setForeground(table.getSelectionForeground());
+				btn.setBackground(table.getSelectionBackground());
+			}else {
+				btn.setForeground(table.getForeground());
+				btn.setBackground(table.getBackground());
+			}
+			if(value==null||value.equals("")) {
+				return null;
+			}else {
+				label = value.toString();
+				btn.setText(label);
+				isPushed = true;
+				return btn;
+			}
 		}
+
+	}
+	
+	private class ButtonRenderer extends JButton implements TableCellRenderer{
+		
+		public ButtonRenderer() {
+			// TODO Auto-generated constructor stub
+			setOpaque(true);
+		}
+
+		@Override
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+				int row, int column) {
+			// TODO Auto-generated method stub
+			if(isSelected) {
+				setForeground(table.getSelectionForeground());
+				setBackground(table.getSelectionBackground());
+			}else {
+				setForeground(table.getForeground());
+				setBackground(UIManager.getColor("Button.background"));
+			}
+			if(value==null||value.equals("")) {
+				return null;
+			}else {
+				setText(value.toString());
+				return this;
+			}
+		}
+
 
 	}
 }
